84bbb5bc069cc87383dd369950617e67216b966d
Marcin Orczyk <orczykm@gmail.com>
2016-02-18 10:51:49 +0000

    It seems the editor did not save some files xD

---
+++ builtins.js
@@ -0,0 +1,220 @@
+var builtins = {};
+
+builtins.$add = function(a) {
+    return function(b) {
+        return a + b
+    }
+}
+
+builtins.$del = function(a) {
+    return function(b) {
+        return a - b
+    }
+}
+
+builtins.$mul = function(a) {
+    return function(b) {
+        return a * b
+    }
+}
+
+builtins.$lt = function(a) {
+    return function(b) {
+        return a < b
+    }
+}
+
+builtins.$gt = function(a) {
+    return function(b) {
+        return a > b
+    }
+}
+
+builtins.$eq = function(a) {
+    return function(b) {
+        return a == b
+    }
+}
+
+builtins.$neq = function(a) {
+    return function(b) {
+        return a != b
+    }
+}
+
+builtins.$and = function(a) {
+    return function(b) {
+        return a && b
+    }
+}
+
+builtins.$or = function(a) {
+    return function(b) {
+        return a || b
+    }
+}
+
+builtins.$concat = function(a) {
+    return function(b) {
+        return a + b
+    }
+}
+
+builtins.empty = {}
+
+builtins.get = function(f) {
+    return function(r) {
+        return r[f]
+    }
+}
+
+builtins.del = function(f) {
+    return function(r) {
+        var out = {};
+        for (var x in r)
+            if (x != f) out[x] = r[x];
+        return out;
+    }
+}
+
+builtins.set = function(f) {
+    return function(a) {
+        return function(r) {
+            var out = {};
+            for (var x in r)
+                out[x] = r[x];
+            out[f] = a;
+            return out;
+        }
+    }
+}
+
+builtins.unsafeStringToInt = function(s) {
+    return Number(s);
+}
+
+builtins.match = function(r) {
+  return function(s) {
+		var m = s.match(r);
+  	return m ? exports.Just(m[0]) : exports.Nothing;
+  };
+}
+
+builtins.drop = function(n) {
+	return function(s) {
+  	return s.substring(n);
+  }
+}
+
+builtins.length = function(s) {
+	return s.length;
+}
+
+builtins.emptyArray = [];
+
+builtins.push = function(x) {
+	return function(xs) {
+  	var xs2 = xs.slice(0);
+    xs2.push(x);
+    return xs2;
+  }
+}
+
+builtins.enqueue = function(x) {
+	return function(xs) {
+  	var xs2 = xs.slice(0);
+    xs2.unshift(x);
+    return xs2;
+  }
+}
+
+builtins.intToString = function(i) {
+	return String(i);
+}
+
+builtins.intercalate = function(sep) {
+	return function(arr) {
+  	if (arr.length == 0) {
+    	return '';
+    }
+    return arr.slice(1).reduce(function(s, x) { return s + sep + x }, arr[0]);
+  }
+}
+
+builtins.slice = function(i) {
+	return function(arr) {
+  	return arr.slice(i);
+  }
+}
+
+builtins.concat = function(a) {
+    return function(b) {
+        return a.concat(b)
+    }
+}
+
+builtins.map = function(f) {
+	return function(arr) {
+  	return arr.map(f)
+  }
+}
+
+builtins.filter = function(f) {
+	return function(arr) {
+  	return arr.filter(f)
+  }
+}
+
+builtins.foldr = function(f) {
+  return function(a) {
+    return function(arr) {
+      return arr.reduceRight(function(b, a) { return f(b)(a) }, a)
+    }
+  }
+}
+
+builtins.foldr1 = function(f) {
+  return function(arr) {
+    return arr.reduceRight(function(b, a) { return f(b)(a) })
+  }
+}
+
+builtins.foldl = function(f) {
+  return function(a) {
+    return function(arr) {
+      return arr.reduce(function(b, a) { return f(b)(a) }, a)
+    }
+  }
+}
+
+builtins.foldl1 = function(f) {
+  return function(arr) {
+    return arr.reduce(function(b, a) { return f(b)(a) })
+  }
+}
+
+builtins.error = function(s) {
+	throw s;
+}
+
+builtins.jsonStringify = function(s) {
+	return JSON.stringify(s);
+}
+
+builtins.iterate = start => isFinished => fun => {
+  var i = 0;
+	var x = start;
+  while (!isFinished(x)) {
+  	x = fun(x);
+    if (i++ > 1000000) {
+    	throw 'infinite loop protection' + i;
+    }
+  }
+  return x;
+}
+
+builtins.True = true;
+
+builtins.False = false;
+
+exports = builtins;
