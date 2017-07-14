a198e5d7a938ec76e0cb5e8f12ecf820e8d14eb2
Marcin Orczyk <orczykm@gmail.com>
2017-07-14 15:39:09 +0000

    Cleaned up the passes a bit to express them cleanly as a sequence of transformations, cleaned up js backend to handle a single module only since that is all we need right now, also exports are now generated based on the AnnExport on the bindings, which gives us a clear path towards explicit exports.

--- compiler/e2e/run.js
+++ compiler/e2e/run.js
@@ -5,17 +5,8 @@ const assert = require('assert');
 const actual = require(process.cwd() + '/' + process.argv[2])
 const expected = require(process.cwd() + '/' + process.argv[3])
 
-const actualKeys = {};
-for (const key in actual) {
-  console.log(key);
-  const m = key.match(/[^$]+$/);
-  if (m) {
-    actualKeys[m[0]] = key;
-  }
-}
-
 for (const key in expected) {
   console.log(`- ${key}`);
   //console.log(actual[key]);
-  assert.deepEqual(actual[actualKeys[key]], expected[key]);
+  assert.deepEqual(actual[key], expected[key]);
 }
